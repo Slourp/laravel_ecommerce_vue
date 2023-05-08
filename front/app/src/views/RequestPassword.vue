d8b85e535796687a2ee735810ea10c0cce64fc53
david vanmak <david.vanmak@pop-france.eu>
2023-05-08 18:25:08 +0200

    [UPDATE] move styled divs to GuestLayoutcomponent

--- front/app/src/views/RequestPassword.vue
+++ front/app/src/views/RequestPassword.vue
@@ -1,39 +1,35 @@
 <template>
-  <div class="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-    <div class="w-full max-w-md space-y-8">
-      <GuestLayout title="
+  <GuestLayout title="
       Request new password
       ">
-        <form class="mt-8 space-y-6" action="#" method="POST">
-          <input type="hidden" name="remember" value="true" />
-          <div class="-space-y-px rounded-md shadow-sm">
-            <div>
-              <label for="email-address" class="sr-only">Email address</label>
-              <input id="email-address" name="email" type="email" autocomplete="email" required="true"
-                class="relative block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                placeholder="Email address" />
-            </div>
-          </div>
+    <form class="mt-8 space-y-6" action="#" method="POST">
+      <input type="hidden" name="remember" value="true" />
+      <div class="-space-y-px rounded-md shadow-sm">
+        <div>
+          <label for="email-address" class="sr-only">Email address</label>
+          <input id="email-address" name="email" type="email" autocomplete="email" required="true"
+            class="relative block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+            placeholder="Email address" />
+        </div>
+      </div>
 
-          <div class="flex items-center justify-between">
-            <div class="text-sm">
-              <router-link :to="{ name: 'login' }" class="font-medium text-indigo-600 hover:text-indigo-500">
-                Remember your password?
-              </router-link>
-            </div>
-          </div>
+      <div class="flex items-center justify-between">
+        <div class="text-sm">
+          <router-link :to="{ name: 'login' }" class="font-medium text-indigo-600 hover:text-indigo-500">
+            Remember your password?
+          </router-link>
+        </div>
+      </div>
 
-          <div>
-            <button type="submit"
-              class="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+      <div>
+        <button type="submit"
+          class="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
 
-              Sign in
-            </button>
-          </div>
-        </form>
-      </GuestLayout>
-    </div>
-  </div>
+          Sign in
+        </button>
+      </div>
+    </form>
+  </GuestLayout>
 </template>
 
 <script setup>
